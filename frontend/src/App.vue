86c1ac017788c5e9c2b5216904d70800700b247c
Lea Delacotte <ldelacotte@smb.sqli.com>
2022-01-04 14:21:40 +0100

    Integration of homepage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <span>
+    <BurgerMenu />
+    <router-view />
+  </span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BurgerMenu from "./components/BurgerMenu.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BurgerMenu,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "style/Global.scss";
+
+@font-face {
+  font-family: "QuickSand-book";
+  src: url("assets/fonts/Quicksand_Book.otf") format("opentype");
+}
+@font-face {
+  font-family: "QuickSand-bold";
+  src: url("assets/fonts/Quicksand_Bold.otf") format("opentype");
+}
+@font-face {
+  font-family: "QuickSand-light";
+  src: url("assets/fonts/Quicksand_Light.otf") format("opentype");
+}
+@font-face {
+  font-family: "QuickSand-dash";
+  src: url("assets/fonts/Quicksand_Dash.otf") format("opentype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: QuickSand-book;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  margin: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 15px;
 }
 </style>
---
+++ frontend/src/components/BurgerMenu.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="burger-menu">
+    <div class="burger-menu__close-button">
+      <svg v-on:click="show = !show">
+        <use class="icon" xlink:href="#burgerMenu" />
+      </svg>
+    </div>
+
+    <transition name="slide-fade">
+      <div class="container" v-if="show">
+        <div class="container__close-button">
+          <svg v-on:click="show = !show">
+            <use class="icon" xlink:href="#close" />
+          </svg>
+        </div>
+        <ul>
+          <router-link class="container__routerlink" to="/" exact>
+            <span class="routerlink__box">
+              <svg v-on:click="show = !show">
+                <use class="icon" xlink:href="#home" />
+              </svg>
+              <span>Accueil</span>
+            </span>
+          </router-link>
+          <router-link class="container__routerlink" to="/research" exact>
+            <span class="routerlink__box">
+              <svg v-on:click="show = !show">
+                <use class="icon" xlink:href="#search" />
+              </svg>
+              <span>Recherche</span>
+            </span></router-link
+          >
+          <router-link class="container__routerlink" to="/inventory" exact>
+            <span class="routerlink__box">
+              <svg v-on:click="show = !show">
+                <use class="icon" xlink:href="#inventory" />
+              </svg>
+              <span>Inventaire</span>
+            </span></router-link
+          >
+          <router-link class="container__routerlink" to="/tools" exact>
+            <span class="routerlink__box">
+              <svg v-on:click="show = !show">
+                <use class="icon" xlink:href="#tool" />
+              </svg>
+              <span>Outils</span>
+            </span></router-link
+          >
+        </ul>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BurgerMenu",
+  components: {},
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/BurgerMenu";
+</style>
