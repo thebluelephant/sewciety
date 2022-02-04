b456af93855816f8f7159c81e08a54ce739d9e8d
Léa <ldelacotte@sqli.com>
2022-02-04 17:26:18 +0100

    Pattern page : Display details of a pattern and create chip menu

--- frontend/src/pages/PatternPage.vue
+++ frontend/src/pages/PatternPage.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="pattern-page">Oh hey</div>
+  <div class="pattern-page" v-if="pattern">
+    <ChipMenu
+      :chips="['Patron', 'Pas à pas', 'Réalisation']"
+      @chip-clicked="setComponentVisibility"
+    />
+    <div v-if="componentDisplay == 'Patron'"  class="pattern">
+      <img class="pattern__image" alt="pattern img" :src="pattern.imageUrl" />
+      <p class="pattern__title title--primary">{{ pattern.name }}</p>
+      <p class="pattern__description">{{ pattern.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-//import { apiCall } from "../services/patterns-api";
+import ChipMenu from "../components/ChipMenu.vue";
+import { apiCall } from "../services/patterns-api";
 
 export default {
-  name: "Pattern",
-  data() {},
-  methods: {},
-  beforeMount() {},
+  components: { ChipMenu },
+  name: "PatternPage",
+  data() {
+    return {
+      pattern: '',
+      componentDisplay: "",
+    };
+  },
+  methods: {
+    fetchPatternData() {
+      apiCall.searchPatternById(this.$route.params.id).then((resp) => {
+        if (resp) {
+          this.pattern = resp;
+        }
+      });
+    },
+    setComponentVisibility(componentName) {
+      this.componentDisplay = componentName;
+    },
+  },
+  beforeMount() {
+    this.fetchPatternData();
+    this.componentDisplay = "Patron";
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../style/Researchpage";
+@import "../style/Patternpage";
 </style>
